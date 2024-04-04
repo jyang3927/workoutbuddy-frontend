36b59bb471883f16af32d3c10c0264bbe206e768
Jamie Yang <yangjm1287@gmail.com>
2024-04-04 11:43:19 -0400

    css styling login and home

--- src/login/components/AuthBar.tsx
+++ src/login/components/AuthBar.tsx
@@ -1,33 +1,39 @@
 import { Button, Container, FormControl } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
-
+import "../../styles/authBar.css"
 export function AuthBar () {
     const {signIn, signOut, user} = useAuth();
     return (
         <div className="AuthBar">
-        {user ?
-        <div>
-            <div>Welcome!
-            <Link to="/calendar"><Button>Calendar</Button></Link> 
-            <Link to="/favorites/routines"><Button>My Routines</Button></Link>
-            <Link to="/favorites/exercises"><Button>My Exercises</Button></Link>     
-            <Button onClick={signOut}>Sign Out</Button>
-            </div>
+            {user ?
             <div>
-                <img src="./img/workout.png" />
-                <p>Trying to ditch archaic methods of tracking workouts?</p>
-                <h4>Well you're in the right place!</h4>
+                <div className="Welcome">
+                    <div className="WelcomeHeader">
+                        <div className="WelcomeName">Welcome {user.displayName}!</div>
+                    </div>
+                    <div className="NavBar">
+                        <Link to="/calendar"><Button className="Nav">Calendar</Button></Link> 
+                        <Link to="/favorites/routines"><Button>My Routines</Button></Link>
+                        <Link to="/favorites/exercises"><Button>My Exercises</Button></Link>  
+                        <Button className="SignOut" size="small" variant="contained" onClick={signOut}>Sign Out</Button>   
+                    </div>
+                </div>
+                <div>
+                    <img className="Logo" src="./img/gym-svgrepo-com.svg" />
+                    <p>Trying to ditch archaic methods of tracking workouts?</p>
+                    <h4>Well you're in the right place!</h4>
+                </div>
             </div>
-        </div>
             :
             <div>
                 <Container>
-                <h1>Welcome to WorkoutBuddy</h1>
-                <h3>The perfect app for recording and favoriting YOUR workouts</h3>
+                <h1 className="WelcomeSign">Welcome to Workout Buddy</h1>
+                <img className="Logo" src="./img/gym-svgrepo-com.svg" />
+                <h3 className="CatchPhrase">The perfect app for recording and favoriting YOUR workouts</h3>
                 <form className="loginSection">
                     <FormControl sx={{ width: '25ch' }}>
-                    <Button onClick={signIn} variant="contained">Sign In With Google</Button>
+                    <Button className="GoogleSignIn" onClick={signIn} variant="contained">Sign In With Google</Button>
                     </FormControl>
                 </form>
                 </Container>
