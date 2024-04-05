7dc1e93abff78e137b28decd151757c4a3816f91
Jack Neelsen <neelsen.jack@gmail.com>
2024-04-05 16:22:27 -0400

    basicExFormStuff

---
+++ src/addExercise/components/SetForm.tsx
@@ -0,0 +1,12 @@
+import TextField from "@mui/material/TextField";
+
+export function SetForm () {
+    return (
+        <div>
+            <form action="">
+                <TextField label="weight"></TextField>
+                <TextField label="reps"></TextField>
+            </form>
+        </div>
+    )
+}
---
+++ src/addExercise/components/SingleExercise.tsx
@@ -0,0 +1,15 @@
+import Exercise from "../../models/Exercise"
+
+interface SingleExerciseProps {
+    exercise:Exercise
+}
+
+export function SingleExercise ({exercise}: SingleExerciseProps) {
+    return (
+        <div>
+            <p>{exercise.name} ({exercise.muscle}): {exercise.type} </p>
+            <button>Add Set</button>
+            <button>Delete Exercise</button>
+        </div>
+    )
+}
