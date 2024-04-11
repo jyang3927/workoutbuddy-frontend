60578ba343e6abd5cd62ad44c8a395a1f3fb0887
Fawaz Butt <fawazbutt@Fawazs-MBP-2.attlocal.net>
2024-04-11 01:32:29 -0400

    added routine favorites button

--- src/services/dataBase/dbExerciseService.ts
+++ src/services/dataBase/dbExerciseService.ts
@@ -2,54 +2,79 @@ import { db } from "../../firebaseConfig";
 import { Exercise } from "../../models/Exercise";
 import dbAxiosInstance from "../helpers/dbAxiosInstance";
 
-export const getExerciseById = async(exerciseId: string): Promise<Exercise> => {
-    try{
-        let response = await dbAxiosInstance.get(`/exercises/${exerciseId}`); 
-        return response.data; 
-    } catch (error:any){
-        console.log("Error failed to fetch data", error); 
-        throw error;
-    }   
-}
+export const getExerciseById = async (
+  exerciseId: string
+): Promise<Exercise> => {
+  try {
+    let response = await dbAxiosInstance.get(`/exercises/${exerciseId}`);
+    return response.data;
+  } catch (error: any) {
+    console.log("Error failed to fetch data", error);
+    throw error;
+  }
+};
 
-export const createNewExercise = async(exercise: Exercise): Promise<Exercise> => {
-    try{
-        let response = await dbAxiosInstance.post(`/exercises`, exercise)
-        return response.data; 
-    }catch (error:any){
-        console.log("Error failed to fetch data", error); 
-        throw error;
-    }   
-}
+export const createNewExercise = async (
+  exercise: Exercise
+): Promise<Exercise> => {
+  try {
+    let response = await dbAxiosInstance.post(`/exercises`, exercise);
+    return response.data;
+  } catch (error: any) {
+    console.log("Error failed to fetch data", error);
+    throw error;
+  }
+};
 
-export const deleteExercise = async(exerciseId:string) : Promise<void> => {
-    try{
-        await dbAxiosInstance.delete(`/exercises/${exerciseId}`); 
-        console.log("successful")
-    }catch (error:any){
-        console.log("Error failed to fetch data", error); 
-        throw error;
-    } 
-}
+export const deleteExercise = async (exerciseId: string): Promise<void> => {
+  try {
+    await dbAxiosInstance.delete(`/exercises/${exerciseId}`);
+    console.log("successful");
+  } catch (error: any) {
+    console.log("Error failed to fetch data", error);
+    throw error;
+  }
+};
 
-export const editExercise = async(exerciseId:string, exerciseUpdate:Partial<Exercise>): Promise<void> => {
-    try{
-        await dbAxiosInstance.put(`/exercises/${exerciseId}`, exerciseUpdate); 
-    }
-    catch(error:any){
-        console.log("Error failed to fetch data", error); 
-        throw error;
-    } 
-}
+export const editExercise = async (
+  exerciseId: string,
+  exerciseUpdate: Partial<Exercise>
+): Promise<void> => {
+  try {
+    await dbAxiosInstance.put(`/exercises/${exerciseId}`, exerciseUpdate);
+  } catch (error: any) {
+    console.log("Error failed to fetch data", error);
+    throw error;
+  }
+};
 
-//create new exercise in routine 
-export const createExerciseInRoutine = async(exercise: Exercise, routineId: string): Promise<Exercise> => {
-    try{
-        let response = await dbAxiosInstance.post(`/exercises/${routineId}`, exercise); 
-        return response.data; 
-    }
-    catch (error:any){
-        console.log("Error failed to fetch data", error); 
-        throw error;
-    }
-}
+//create new exercise in routine
+export const createExerciseInRoutine = async (
+  exercise: Exercise,
+  routineId: string
+): Promise<Exercise> => {
+  try {
+    let response = await dbAxiosInstance.post(
+      `/exercises/${routineId}`,
+      exercise
+    );
+    return response.data;
+  } catch (error: any) {
+    console.log("Error failed to fetch data", error);
+    throw error;
+  }
+};
+export const addExerciseIdToRoutine = async (
+  exercise: Exercise,
+  routineId: string
+) => {
+  try {
+    const response = await dbAxiosInstance.patch(`/routines/${routineId}`, {
+      exercise,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error updating routine with new exercise ID", error);
+    throw error;
+  }
+};
